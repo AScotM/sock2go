5e5d9332bb7296895187ecaf2f6234666fa10595
Arnoldas Skinderis <102791583+AScotM@users.noreply.github.com>
2025-03-19 09:32:40 +0200

    Update sock2.go

--- sock2.go
+++ sock2.go
@@ -2,38 +2,38 @@ package main
 
 import (
     "fmt"
-    "os/exec"
     "log"
+    "os/exec"
 )
 
 func getSocketStatistics() (string, error) {
-    // Check if ss command is available
-    _, err := exec.LookPath("ss")
-    if err != nil {
+    // Check if the "ss" command is available
+    if _, err := exec.LookPath("ss"); err != nil {
         return "", fmt.Errorf("ss command not found: %v", err)
     }
 
+    // Execute the "ss -s" command
     cmd := exec.Command("ss", "-s")
-    output, err := cmd.Output()
+    output, err := cmd.CombinedOutput() // Captures both stdout and stderr
     if err != nil {
-        return "", fmt.Errorf("failed to execute ss command: %v", err)
+        return "", fmt.Errorf("failed to execute ss command: %v, output: %s", err, string(output))
     }
+
     return string(output), nil
 }
 
 func printSocketStatistics() error {
     socketStats, err := getSocketStatistics()
     if err != nil {
-        return err // Error will be logged in main
+        return err // Logging will be handled in main
     }
 
-    fmt.Println("Socket Statistics:")
-    fmt.Println(socketStats)
+    fmt.Println("Socket Statistics:\n" + socketStats)
     return nil
 }
 
 func main() {
     if err := printSocketStatistics(); err != nil {
-        log.Printf("Failed to get socket statistics: %v\n", err)
+        log.Fatalf("Failed to get socket statistics: %v", err) // Terminates on failure
     }
 }
